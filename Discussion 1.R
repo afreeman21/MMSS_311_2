0351f87f9c808926b18da5739b92531a937efef7
Alex Freeman <alexanderfreeman2021@u.northwestern.edu>
2019-04-05 15:28:34 -0500

    Update Discussion 1.R

--- Discussion 1.R
+++ Discussion 1.R
@@ -2,5 +2,10 @@ setwd("~/Documents/GitHub/MMSS_311_2")
 
 install.packages(c("dplyr", "ggplot2", "stringr", "lubridate"))
 
+qog <-read_excel("http://www.qogdata.pol.gu.se/data/qog_std_cs_jan19.xlsx")
+
 print(dim(qog_std_cs_jan19))
+
+
+
 
